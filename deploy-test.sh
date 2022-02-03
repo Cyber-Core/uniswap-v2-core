6155a7dd94957efd6d4e1beef56c8c2783a68ec7
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-02-03 18:07:04 +0300

    Bugfix (320-change-new-user-airdrop)

--- deploy-test.sh
+++ deploy-test.sh
@@ -4,9 +4,8 @@ set -euo pipefail
 TESTSET=${1:-}
 echo "\$TESTSET='$TESTSET'"
 
-if [[ -z "$FAUCET_URL" ]]
+if [[ !-z "$FAUCET_URL" ]]
 then
-else
   curl -i -X POST -H "Content-Type: text/plain" "$FAUCET_URL/request_neon" -d '{ "wallet": "0xaA4d6f4FF831181A2bBfD4d62260DabDeA964fF1", "amount": 5 }'
   curl -i -X POST -H "Content-Type: text/plain" "$FAUCET_URL/request_neon" -d '{ "wallet": "0x6970d087e7e78A13Ea562296edb05f4BB64D5c2E", "amount": 5 }'
 fi

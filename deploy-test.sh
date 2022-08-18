7971ee65fca01be7ab9328353e69191e04fb525c
Andrew Falaleev <andrew.falaleev@gmail.com>
2022-08-19 00:00:06 +0300

    Increase airdrop for real gas prices

--- deploy-test.sh
+++ deploy-test.sh
@@ -8,14 +8,17 @@ if [[ -z "$FAUCET_URL" ]]; then
     echo 'FAUCET_URL is not set'
     exit 1
 else
-    curl -i -X POST -H "Content-Type: text/plain" "$FAUCET_URL/request_neon" -d '{ "wallet": "0xaA4d6f4FF831181A2bBfD4d62260DabDeA964fF1", "amount": 100 }'
-    curl -i -X POST -H "Content-Type: text/plain" "$FAUCET_URL/request_neon" -d '{ "wallet": "0x6970d087e7e78A13Ea562296edb05f4BB64D5c2E", "amount": 100 }'
+    for ADDRESS in 0xaA4d6f4FF831181A2bBfD4d62260DabDeA964fF1 0x6970d087e7e78A13Ea562296edb05f4BB64D5c2E; do
+        REQUEST='{ "wallet": "'$ADDRESS'", "amount": 5000 }'
+        echo curl -i -X POST -H "Content-Type: text/plain" "$FAUCET_URL/request_neon" -d "$REQUEST"
+        curl -i -X POST -H "Content-Type: text/plain" "$FAUCET_URL/request_neon" -d "$REQUEST"
+    done
 fi
 
 if [ "$TESTSET" = "all" ]; then
     yarn test
 else
-    node node_modules/mocha/bin/mocha --grep "^UniswapV2Pair swap:token0$"
+    node node_modules/mocha/bin/mocha --grep "^$TESTSET"
 fi
 
 exit 0
